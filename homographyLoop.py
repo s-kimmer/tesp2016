255592d5786c2af4e639be104122c2011a1376a8
Stefan Kimmer <stefan.kimmer@esa.int>
2016-07-29 09:56:02 +0200

    Inverted Homography

--- homographyLoop.py
+++ homographyLoop.py
@@ -62,8 +62,10 @@ UVcenter = np.array([[320], [240], [1]])
 
 imgIndex = imgStartIndex
 
-while True:
+doLoop = True
+while doLoop:
     cv2.imshow("projector", imgProj)    
+    
     
     if option == 1:
         # wEB CAM
@@ -78,6 +80,9 @@ while True:
         frame = cv2.imread(imgFilePath, 1)
         imgIndex = imgIndex + 1
         
+        
+    
+    
     #ORB detector
     cv2.ocl.setUseOpenCL(False) #bugfix
     kpframe = orb.detect(frame,None) #find the keypoints with ORB
@@ -100,7 +105,7 @@ while True:
     #homography    
     H, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
     
-    UV = np.dot(H, UVcenter) #H or H^-1 ???
+    UV = np.dot(np.linalg.inv(H), UVcenter) #H or H^-1 ???
     UVconv = np.array([[0], [0]])
     UVconv[0] = max(0 + 0.5 * wOL, UV[0])
     UVconv[1] = max(0 + 0.5 * hOL, UV[1])
@@ -111,12 +116,19 @@ while True:
     #imgProj = np.zeros(h1, w1, np.uint8)
     imgProj = imgBG
     imgProj[(np.int(UVconv[1] - 0.5 * hOL)):np.int((UVconv[1] + 0.5 * hOL)), np.int((UVconv[0] - 0.5 * wOL)):np.int((UVconv[0] + 0.5 * wOL))] = imgOL
-# This does not work:
-# new comment    
-    #imgProj = cv2.cvtColor(imgProj, cv2.COLOR_GRAY2BGR)
-#    
-    key = cv2.waitKey(2)
-    if key == 27:
-        cv2.destroyAllWindows()
-        break
 
+
+    # Show orig frame
+    #cv2.
+    cv2.imshow("camera", frame)
+
+    key = cv2.waitKey(2)
+
+    while key != 13:
+        key = cv2.waitKey(2)
+        if key == 27:
+            cv2.destroyAllWindows()
+            doLoop = False
+            break
+
+        
