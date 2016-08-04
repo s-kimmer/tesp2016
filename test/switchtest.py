ba961abfeb8301127727b7aec8f809b1ded194a1
mandydydy <mandyhuangm@gmail.com>
2016-08-04 17:08:14 +0900

    new switch

--- test/switchtest.py
+++ test/switchtest.py
@@ -13,12 +13,12 @@ import numpy as np
 option = 0 #choose 0 for files, 1 for webcam
 
 #image Settings
-imgOL_path = ".../src/layerImgs/pikachu.jpg"
-imgBG_path = "../src/layerImgs/original.png"
-camera_device_index = 0 #choose camera device [0,N-1], 0 for first device, 1 for second device etc.
+imgOL_path = "./tesp2016/src/pikachu.jpg"
+imgBG_path = "./tesp2016/src/original.png"
+camera_device_index = 1 #choose camera device [0,N-1], 0 for first device, 1 for second device etc.
 
 # For captured camera images:
-imgSourceDir = "../src/imgs5"
+imgSourceDir = "./tesp2016/src/imgs5"
 imgFileNameDesc = "img%03d.png"
 imgStartIndex = 0
 imgEndIndex = 400
@@ -66,58 +66,65 @@ doLoop = True
 
 RotIndOrig = np.array([[320,320],[0,480],[1,1]])
 Rot = 0
+counter = 0
+numOfH = 0
+flag = 0
+mode = 0
 # if no rotation, Rot = 0; if anticlockwise rotation(90+-30), Rot = 1, if 
 #clockwise rotation(90+-30), Rot = 2; else Rot = 3 
 
-def rotation(H,RotIndOrig):        #Rotation dection
-    RotIndProj = np.dot(np.linalg.inv(H), RotIndOrig)
-    RotDis = RotIndProj[1,0] - RotIndProj[1,1]
-    if np.abs(RotDis) < 240:#240 = 480*sin30
-        if RotIndProj[0,0] < RotIndProj[0,1]:
-            Rot = 1 #anticlockwise rotation
-        else:
-            Rot = 2 #clockwise rotation
-    elif np.abs(RotDis) > 339:#339 = 480*cos30
+def rotation(H, RotIndOrig):        #Rotation dection
+    RotIndProj = np.dot(H, RotIndOrig)
+
+    RotIndx1 = RotIndProj[0,0]/RotIndProj[2,0] 
+    RotIndx2 = RotIndProj[0,1]/RotIndProj[2,1]
+    RotIndy1 = RotIndProj[1,0]/RotIndProj[2,0]
+    RotIndy2 = RotIndProj[1,1]/RotIndProj[2,1]
+    tanAngle = np.abs(RotIndx1-RotIndx2)/max(0.1,np.abs(RotIndy1-RotIndy2))
+    if tanAngle < 0.57:# tan30
         Rot = 0
+    elif tanAngle > 1.707: #tan30
+        Rot = 1
     else:
-        Rot = 3
+        Rot = 2
+#    print RotIndx1,RotIndy1,RotIndx2,RotIndy2
+#    print "tanAngle = %f" % tanAngle
 #    print Rot
     return Rot
-
-mode = 0 #mode
-numOfH = 0 #number of 'high' states
-counter = 0 #noise toleration 
-def switch(Rot,counter,numOfH,mode):
+    
+    
+# mode switch
+def switch(Rot, counter, numOfH, mode,flag):
     if Rot == 1:
         counter = 3
     elif counter > 0 :
-        counter = counter - 1
-                
+        counter = counter - 1  
+             
     if counter > 0:
         numOfH = numOfH + 1
     else:
-        numOfH = 0
-       
-    print "Rot = %d" % Rot
-    print "numOfH = %d"%numOfH        
+        numOfH = 0        
+        
+    #print "Rot = %d" % Rot
+    #print "numOfH = %d"%numOfH        
     
-    if numOfH >= 15:        
-        if mode >= 3:# 3 modes
+    if numOfH > 7:
+        flag =1
+
+#    print "Rot = %d" % Rot
+#    print "numOfH = %d"%numOfH
+#    print "flag = %d"%flag    
+    if flag == 1 and Rot == 0:        
+        
+        if mode >= 3:# 4 modes
             mode = 0
         else:
             mode = mode + 1
-        numOfH = 0
-        counter = 0            
-
-    print "mode = %d\n" % mode
-    return (mode,counter,numOfH)        
-
-
-# Create Window for fullscreen display
-cv2.namedWindow("projector", cv2.WND_PROP_FULLSCREEN)          
-cv2.setWindowProperty("projector", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-cv2.moveWindow("projector", 1920, 0)
-
+        print "mode = %d\n" % mode
+        counter = 0
+        numOfH = 0   
+        flag = 0         
+    return (mode,counter,numOfH,flag)        
 
 while doLoop:
     cv2.imshow("projector", imgProj)    
@@ -176,7 +183,7 @@ while doLoop:
     
     # Rotation detection
     Rot = rotation(H,RotIndOrig)
-    mode,counter,numOfH = switch(Rot,counter,numOfH,mode)
+    mode,counter,numOfH,flag = switch(Rot,counter,numOfH,mode,flag)
     # Show orig frame
 
     cv2.imshow("camera", frame)
